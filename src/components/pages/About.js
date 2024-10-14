39d81f1a314e522c130bfd6287bc122685667dc6
yongsin <dbtlawleh@gmail.com>
2024-10-14 23:40:27 +0900

    실제 내용으로 변경 작업

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,19 +11,20 @@ const About = () => {
       <div className="cont-about">
         <div className="left">
           <h3 className="title">Publishing & Frontend Developer</h3>
-          <p className="desc">
-            Dolor reprehenderit aut repudiandae maiores recusandae. Consequatur
-            dolor nisi labore commodi ipsa? Excepturi velit laboriosam at
-            voluptatibus cupiditate est vel. Dolorem praesentium itaque fugiat
-            placeat quas consequatur? Obcaecati natus rerum
+          <p className="desc intro">
+            안녕하세요! 저는 프론트엔드 개발자로 활동하고 있는 허용신입니다.
           </p>
           <p className="desc">
-            Consectetur consectetur nesciunt nesciunt blanditiis amet? Quod quis
-            unde aliquid excepturi laudantium? Illo architecto aliquam velit
-            illo magnam neque perferendis sint Incidunt eius ipsa inventore
-            labore eos velit. Minus hic magni reiciendis necessitatibus ex
-            Quidem officia deleniti accusamus obcaecati dolores. Porro culpa
-            quibusdam impedit porro praesentium excepturi. Alias ab consequatur?
+            웹 개발 분야에서 약 2년간 경험을 쌓으며, 항상 사용자에게 더 나은
+            경험을 제공하는 것을 목표로 하고 있습니다.
+          </p>
+          <p className="desc">
+            특히, 다양한 기기와 화면 크기에 맞춰 최적화된 반응형 웹을 개발하는
+            데 큰 관심이 있습니다.
+          </p>
+          <p className="desc">
+            저의 목표는 언제나 사용자가 만족할 수 있는 서비스와 화면을 만드는
+            것이며, 이를 위해 지속적으로 성장하고 도전해 나가고 있습니다.
           </p>
         </div>
         <div className="right">
@@ -32,27 +33,22 @@ const About = () => {
             <li className="r_item">
               <span>Name</span>
               <span>:</span>
-              <span> Philip Watson</span>
+              <span>허용신</span>
             </li>
             <li className="r_item">
               <span>Age</span>
               <span>:</span>
-              <span>26</span>
+              <span>30</span>
             </li>
             <li className="r_item">
               <span>Residence</span>
               <span>:</span>
-              <span>New York, USA</span>
-            </li>
-            <li className="r_item">
-              <span>Address</span>
-              <span>:</span>
-              <span>123 Lorem Ipsum</span>
+              <span>서울</span>
             </li>
             <li className="r_item">
               <span>Email</span>
               <span>:</span>
-              <span>example@example.com</span>
+              <span>dbtlawleh@gmail.com</span>
             </li>
             <li className="r_item">
               <span>Phone</span>
@@ -64,35 +60,31 @@ const About = () => {
       </div>
       <div className="job-about">
         <div className="title_wrap">
-          <h2 className="title">job performance</h2>
+          <h2 className="title">Job Experience</h2>
         </div>
         <div className="cont-wrap">
           <div className="item">
             <span className="icon"></span>
             <h4 className="role_name">Web Development</h4>
+            <p className="desc">DMS 솔루션, 퍼블리싱 및 화면 개발</p>
             <p className="desc">
-              Minus hic magni reiciendis necessitatibus ex Quidem officia
-              deleniti accusamus obcaecati dolores. Porro culpa quibusdam
-              impedit porro praesentium excepturi. Alias ab consequatur?
+              전자문서 비식별화 솔루션, 퍼블리싱 및 화면 개발
             </p>
           </div>
           <div className="item">
             <span className="icon"></span>
             <h4 className="role_name">UX/UI Design</h4>
             <p className="desc">
-              Minus hic magni reiciendis necessitatibus ex Quidem officia
-              deleniti accusamus obcaecati dolores. Porro culpa quibusdam
-              impedit porro praesentium excepturi. Alias ab consequatur?
+              DMS 솔루션, 와이어프레임 및 프로토타이핑 작성
             </p>
+            <p className="desc">문서 뷰어 솔루션, UX/UI 디자인</p>
+            <p className="desc">전자문서 비식별화 솔루션, UX/UI 디자인</p>
           </div>
           <div className="item">
             <span className="icon"></span>
             <h4 className="role_name">Service Planning</h4>
-            <p className="desc">
-              Minus hic magni reiciendis necessitatibus ex Quidem officia
-              deleniti accusamus obcaecati dolores. Porro culpa quibusdam
-              impedit porro praesentium excepturi. Alias ab consequatur?
-            </p>
+            <p className="desc">SaaS 제품 서비스 기획 및 운영</p>
+            <p className="desc">전자문서 비식별화 솔루션, 화면설계서 작성</p>
           </div>
         </div>
       </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -14,18 +14,18 @@ const Resume = () => {
         <div className="cont-resume">
           <div className="left">
             <div className="title_wrap">
-              <h3 className="title">EXPERIENCE</h3>
+              <h3 className="title">CAREER</h3>
             </div>
             <ul className="experience">
               <li>
                 <span className="line-left"></span>
                 <div className="content">
-                  <h4>Lead UI Designer</h4>
-                  <h5>Big Design Company, NY, USA</h5>
+                  <h4>연구 개발부</h4>
+                  <h5>(주) 에스에이티정보</h5>
                   <p className="info">
-                    Sit cupiditate praesentium ex esse nulla Facere fuga
-                    perspiciatis eveniet provident neque Ea ratione non minus
-                    temporibus ipsum Sunt minima
+                    자사 소프트웨어 솔루션 개발 과정에서의 UX/UI 디자인과
+                    프로토타이핑부터 웹 개발까지 다양한 업무를 진행한 경험이
+                    있습니다.
                   </p>
                 </div>
                 <div className="year">
@@ -36,12 +36,12 @@ const Resume = () => {
               <li>
                 <span className="line-left"></span>
                 <div className="content">
-                  <h4>Lead UI Designer</h4>
-                  <h5>Big Design Company, NY, USA</h5>
+                  <h4>촬영 지원부</h4>
+                  <h5>(주) 기발한사람들</h5>
                   <p className="info">
-                    Sit cupiditate praesentium ex esse nulla Facere fuga
-                    perspiciatis eveniet provident neque Ea ratione non minus
-                    temporibus ipsum Sunt minima
+                    영상 제작업체를 대상으로 인력 매칭 서비스와 촬영 기자재 납품
+                    등의 서비스를 운영하였으며, 서비스 운영의 영역을 확장하기
+                    위해 SaaS 제품을 기획하고 운영한 경험이 있습니다.
                   </p>
                 </div>
                 <div className="year">
@@ -60,12 +60,11 @@ const Resume = () => {
               <li>
                 <span className="line-left"></span>
                 <div className="content">
-                  <h4>Lead UI Designer</h4>
-                  <h5>Big Design Company, NY, USA</h5>
+                  <h4>그린컴퓨터 아카데미</h4>
+                  <h5>UX/UI 기획 & 퍼블리싱</h5>
                   <p className="info">
-                    Sit cupiditate praesentium ex esse nulla Facere fuga
-                    perspiciatis eveniet provident neque Ea ratione non minus
-                    temporibus ipsum Sunt minima
+                    스케치 도구를 활용한 UX/UI 디자인 기술과 웹 퍼블리싱을 위한
+                    HTML, CSS, Javascript 언어를 학습했습니다.
                   </p>
                 </div>
                 <div className="year">
@@ -76,12 +75,11 @@ const Resume = () => {
               <li>
                 <span className="line-left"></span>
                 <div className="content">
-                  <h4>Lead UI Designer</h4>
-                  <h5>Big Design Company, NY, USA</h5>
+                  <h4>청운대학교</h4>
+                  <h5>영화학과</h5>
                   <p className="info">
-                    Sit cupiditate praesentium ex esse nulla Facere fuga
-                    perspiciatis eveniet provident neque Ea ratione non minus
-                    temporibus ipsum Sunt minima
+                    카메라 전공으로서, 영상 미디어 제작에 대한 전반적인 지식과
+                    카메라 활용 기술을 학습했습니다.
                   </p>
                 </div>
                 <div className="year">
@@ -98,18 +96,18 @@ const Resume = () => {
               <h3 className="title">Planning Skill</h3>
             </div>
             <SkillBar skill="EXCEL" percentage={70} />
-            <SkillBar skill="POWERPOINT" percentage={55} />
-            <SkillBar skill="PHOTOSHOP" percentage={40} />
-            <SkillBar skill="FIGMA" percentage={85} />
+            <SkillBar skill="POWERPOINT" percentage={70} />
+            <SkillBar skill="PHOTOSHOP" percentage={50} />
+            <SkillBar skill="FIGMA" percentage={80} />
           </div>
           <div className="right">
             <div className="title_wrap">
               <h3 className="title">Coding Skill</h3>
             </div>
-            <SkillBar skill="HTML" percentage={90} />
-            <SkillBar skill="CSS / SCSS" percentage={95} />
-            <SkillBar skill="JavaScript / j-Query" percentage={70} />
-            <SkillBar skill="REACT / REDUX" percentage={60} />
+            <SkillBar skill="HTML" percentage={80} />
+            <SkillBar skill="CSS / SCSS" percentage={90} />
+            <SkillBar skill="JavaScript / j-Query" percentage={80} />
+            <SkillBar skill="REACT / REDUX" percentage={80} />
           </div>
         </div>
       </div>
