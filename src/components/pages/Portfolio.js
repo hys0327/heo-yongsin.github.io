518d6e8516796d9a72b2f1cbb1f6ea14669793a4
yongsin <dbtlawleh@gmail.com>
2024-10-16 16:59:14 +0900

    Portfolio-work 레이아웃 및 산출물 1개 완성

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NewWindowPortal from "../common/NewWindowPortal"; // 새 창을 띄우는 포탈 컴포넌트
 // 실제 프로젝트의 컴포넌트를 import
 import KKST from "../../portfolio-work/responsive/project-A/KKST";
-// 만약 다른 프로젝트가 있으면 여기서 추가로 import
+import "../../styles/scss/responsive_portfolio.scss";
 
 const Portfolio = () => {
   const [selectedWork, setSelectedWork] = useState(null);
@@ -52,10 +52,218 @@ const Portfolio = () => {
       </div>
       <div className="work-item">
         <div className="left">
-          <h3>KKST 화면 개선</h3>
           <div className="img"></div>
         </div>
         <div className="right">
+          <div className="project-info">
+            <div className="title-wrap">
+              <span className="text">프로젝트 명</span>
+              <span className="text">:</span>
+              <span className="text">KKST 화면 개선</span>
+            </div>
+            <div className="category">
+              <span className="text">프로젝트 구분</span>
+              <span className="text">:</span>
+              <span className="text">개인 프로젝트</span>
+            </div>
+            <div className="desc">
+              <h4 className="title">프로젝트 내용</h4>
+              <div className="text-wrap">
+                <p className="text">
+                  이 프로젝트는 지그재그, 무신사등의 타사 제품을 벤치마킹하여
+                  리디자인 및 기능을 접목한 프로젝트입니다.
+                </p>
+                <p className="text">
+                  지그재그의 상품 구매 팝업, 무신사의 구매 내역 선택 기능 등을
+                  적용하여 사용성을 높이고자 했습니다.
+                </p>
+              </div>
+            </div>
+            <div className="skill">
+              <h4 className="title">사용 스택</h4>
+              <p className="text">HTML, CSS, SCSS, JavaScript, React, Redux</p>
+            </div>
+          </div>
+          <div className="submenu">
+            <button
+              className="submenu-btn"
+              onClick={() => {
+                setSelectedWork("ProjectA");
+                setShowMobileView(true);
+              }}
+            >
+              Mobile View
+            </button>
+            <button
+              className="submenu-btn"
+              onClick={() => {
+                if (window.innerWidth <= 932) {
+                  alert("데스크탑 환경에서만 가능합니다.");
+                } else {
+                  setSelectedWork("ProjectA");
+                  setShowDesktopView(true);
+                }
+              }}
+            >
+              Desktop View
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="work-item">
+        <div className="left">
+          <div className="img"></div>
+        </div>
+        <div className="right">
+          <div className="project-info">
+            <div className="title-wrap">
+              <span className="text">프로젝트 명</span>
+              <span className="text">:</span>
+              <span className="text">KKST 화면 개선</span>
+            </div>
+            <div className="category">
+              <span className="text">프로젝트 구분</span>
+              <span className="text">:</span>
+              <span className="text">개인 프로젝트</span>
+            </div>
+            <div className="desc">
+              <h4 className="title">프로젝트 내용</h4>
+              <div className="text-wrap">
+                <p className="text">
+                  이 프로젝트는 지그재그, 무신사등의 타사 제품을 벤치마킹하여
+                  리디자인 및 기능을 접목한 프로젝트입니다.
+                </p>
+                <p className="text">
+                  지그재그의 상품 구매 팝업, 무신사의 구매 내역 선택 기능 등을
+                  적용하여 사용성을 높이고자 했습니다.
+                </p>
+              </div>
+            </div>
+            <div className="skill">
+              <h4 className="title">사용 스택</h4>
+              <p className="text">HTML, CSS, SCSS, JavaScript, React, Redux</p>
+            </div>
+          </div>
+          <div className="submenu">
+            <button
+              className="submenu-btn"
+              onClick={() => {
+                setSelectedWork("ProjectA");
+                setShowMobileView(true);
+              }}
+            >
+              Mobile View
+            </button>
+            <button
+              className="submenu-btn"
+              onClick={() => {
+                if (window.innerWidth <= 932) {
+                  alert("데스크탑 환경에서만 가능합니다.");
+                } else {
+                  setSelectedWork("ProjectA");
+                  setShowDesktopView(true);
+                }
+              }}
+            >
+              Desktop View
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="work-item">
+        <div className="left">
+          <div className="img"></div>
+        </div>
+        <div className="right">
+          <div className="project-info">
+            <div className="title-wrap">
+              <span className="text">프로젝트 명</span>
+              <span className="text">:</span>
+              <span className="text">KKST 화면 개선</span>
+            </div>
+            <div className="category">
+              <span className="text">프로젝트 구분</span>
+              <span className="text">:</span>
+              <span className="text">개인 프로젝트</span>
+            </div>
+            <div className="desc">
+              <h4 className="title">프로젝트 내용</h4>
+              <div className="text-wrap">
+                <p className="text">
+                  이 프로젝트는 지그재그, 무신사등의 타사 제품을 벤치마킹하여
+                  리디자인 및 기능을 접목한 프로젝트입니다.
+                </p>
+                <p className="text">
+                  지그재그의 상품 구매 팝업, 무신사의 구매 내역 선택 기능 등을
+                  적용하여 사용성을 높이고자 했습니다.
+                </p>
+              </div>
+            </div>
+            <div className="skill">
+              <h4 className="title">사용 스택</h4>
+              <p className="text">HTML, CSS, SCSS, JavaScript, React, Redux</p>
+            </div>
+          </div>
+          <div className="submenu">
+            <button
+              className="submenu-btn"
+              onClick={() => {
+                setSelectedWork("ProjectA");
+                setShowMobileView(true);
+              }}
+            >
+              Mobile View
+            </button>
+            <button
+              className="submenu-btn"
+              onClick={() => {
+                if (window.innerWidth <= 932) {
+                  alert("데스크탑 환경에서만 가능합니다.");
+                } else {
+                  setSelectedWork("ProjectA");
+                  setShowDesktopView(true);
+                }
+              }}
+            >
+              Desktop View
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="work-item">
+        <div className="left">
+          <div className="img"></div>
+        </div>
+        <div className="right">
+          <div className="project-info">
+            <div className="title-wrap">
+              <span className="text">프로젝트 명</span>
+              <span className="text">:</span>
+              <span className="text">KKST 화면 개선</span>
+            </div>
+            <div className="category">
+              <span className="text">프로젝트 구분</span>
+              <span className="text">:</span>
+              <span className="text">개인 프로젝트</span>
+            </div>
+            <div className="desc">
+              <h4 className="title">프로젝트 내용</h4>
+              <div className="text-wrap">
+                <p className="text">
+                  이 프로젝트는 지그재그, 무신사등의 타사 제품을 벤치마킹하여
+                  리디자인 및 기능을 접목한 프로젝트입니다.
+                </p>
+                <p className="text">
+                  지그재그의 상품 구매 팝업, 무신사의 구매 내역 선택 기능 등을
+                  적용하여 사용성을 높이고자 했습니다.
+                </p>
+              </div>
+            </div>
+            <div className="skill">
+              <h4 className="title">사용 스택</h4>
+              <p className="text">HTML, CSS, SCSS, JavaScript, React, Redux</p>
+            </div>
+          </div>
           <div className="submenu">
             <button
               className="submenu-btn"
