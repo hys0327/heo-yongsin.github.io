38421d87aba0814db6378fdca615708ced5d5675
yongsin <dbtlawleh@gmail.com>
2024-11-13 18:53:15 +0900

    외부프로젝트 경로변경

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import NewWindowPortal from "../common/NewWindowPortal"; // 새 창을 띄우는 포탈 컴포넌트
+import React, { useState, useEffect } from 'react';
+import NewWindowPortal from '../common/NewWindowPortal'; // 새 창을 띄우는 포탈 컴포넌트
 // 실제 프로젝트의 컴포넌트를 import
-import KKST from "../../portfolio-work/responsive/project-A/KKST";
-import AppNetflix from "../../portfolio-work/responsive/project-B/AppNetflix";
-import "../../styles/scss/responsive_portfolio.scss";
+import KKST from '../../portfolio-work/responsive/project-A/KKST';
+import AppNetflix from '../../portfolio-work/responsive/project-B/AppNetflix';
+import '../../styles/scss/responsive_portfolio.scss';
 
 const Portfolio = () => {
   const [selectedWork, setSelectedWork] = useState(null);
   const [showMobileView, setShowMobileView] = useState(false);
   const [showDesktopView, setShowDesktopView] = useState(false);
-  const [filter, setFilter] = useState("all");
+  const [filter, setFilter] = useState('all');
 
   // 창을 닫을 때 상태를 리셋하는 함수
   const handleWindowClose = () => {
@@ -20,10 +20,10 @@ const Portfolio = () => {
   // 새 창에 띄울 작업물 결정 (Mobile View)
   const renderMobileComponent = () => {
     switch (selectedWork) {
-      case "KKST":
-        return <KKST viewType="mobile" />;
-      case "Netflix":
-        return <AppNetflix viewType="mobile" />;
+      case 'KKST':
+        return <KKST viewType='mobile' />;
+      case 'Netflix':
+        return <AppNetflix viewType='mobile' />;
       default:
         return null;
     }
@@ -32,10 +32,10 @@ const Portfolio = () => {
   // 새 창에 띄울 작업물 결정 (Desktop View)
   const renderDesktopComponent = () => {
     switch (selectedWork) {
-      case "KKST":
-        return <KKST viewType="desktop" />;
-      case "Netflix":
-        return <AppNetflix viewType="desktop" />;
+      case 'KKST':
+        return <KKST viewType='desktop' />;
+      case 'Netflix':
+        return <AppNetflix viewType='desktop' />;
       default:
         return null;
     }
@@ -43,67 +43,67 @@ const Portfolio = () => {
 
   const projects = [
     {
-      id: "KKST",
-      type: "personal",
-      label: "KKST 화면 개선",
+      id: 'KKST',
+      type: 'personal',
+      label: 'KKST 화면 개선',
       description: [
-        "지그재그, 무신사등의 타사 제품을 벤치마킹하여 리디자인 및 기능을 접목한 프로젝트입니다.",
-        "지그재그의 상품 구매 팝업, 무신사의 구매 내역 선택 기능 등을 적용하여 사용성을 높이고자 했습니다.",
+        '지그재그, 무신사등의 타사 제품을 벤치마킹하여 리디자인 및 기능을 접목한 프로젝트입니다.',
+        '지그재그의 상품 구매 팝업, 무신사의 구매 내역 선택 기능 등을 적용하여 사용성을 높이고자 했습니다.',
       ],
-      stack: "HTML, CSS, SCSS, JavaScript, React, Redux",
-      directory: "current",
+      stack: 'HTML, CSS, SCSS, JavaScript, React, Redux',
+      directory: 'current',
     },
     {
-      id: "Netflix",
-      type: "personal",
-      label: "Netflix 리디자인",
+      id: 'Netflix',
+      type: 'personal',
+      label: 'Netflix 리디자인',
       description: [
-        "세로로 이어지는 무한 스크롤 방식은 콘텐츠가 화면 하단에 배치될수록 사용자에게 불편함을 초래할 수 있습니다.",
-        "또한, 화면의 명확한 섹션 구분이 없어, 가독성이 떨어진다고 판단하였습니다.",
+        '세로로 이어지는 무한 스크롤 방식은 콘텐츠가 화면 하단에 배치될수록 사용자에게 불편함을 초래할 수 있습니다.',
+        '또한, 화면의 명확한 섹션 구분이 없어, 가독성이 떨어진다고 판단하였습니다.',
       ],
-      stack: "HTML, CSS, SCSS, JavaScript, React, Redux",
-      directory: "current",
+      stack: 'HTML, CSS, SCSS, JavaScript, React, Redux',
+      directory: 'current',
     },
     {
-      id: "DocuGuard",
-      type: "collaborative",
-      label: "DocuGuard",
+      id: 'DocuGuard',
+      type: 'collaborative',
+      label: 'DocuGuard',
       description: [
-        "2023.07 ~ 2024.05",
-        "Figma를 활용하여, 각 화면에 대한 UI/UX 설계 및 퍼블리싱 (기여도 100%)",
-        "로그인, 문서 관리, 사용자 관리, 이벤트 로그조회, 문서 뷰어 화면 개발 (기여도 60%)",
+        '2023.07 ~ 2024.05',
+        'Figma를 활용하여, 각 화면에 대한 UI/UX 설계 및 퍼블리싱 (기여도 100%)',
+        '로그인, 문서 관리, 사용자 관리, 이벤트 로그조회, 문서 뷰어 화면 개발 (기여도 60%)',
       ],
-      stack: "HTML, CSS, SCSS, JavaScript, React, Redux, Redux-Saga",
+      stack: 'HTML, CSS, SCSS, JavaScript, React, Redux, Redux-Saga',
       images: [
-        "DocuGuard1",
-        "DocuGuard2",
-        "DocuGuard3",
-        "DocuGuard4",
-        "DocuGuard5",
-        "DocuGuard6",
+        'DocuGuard1',
+        'DocuGuard2',
+        'DocuGuard3',
+        'DocuGuard4',
+        'DocuGuard5',
+        'DocuGuard6',
       ],
-      directory: "current",
+      directory: 'current',
     },
     {
-      id: "shinhan",
-      type: "personal",
-      label: "Shinhan Bank 리디자인",
+      id: 'shinhan',
+      type: 'personal',
+      label: 'Shinhan Bank 리디자인',
       description: [
-        "신한은행 웹사이트를 리디자인하여 재구성한 웹사이트 입니다.",
-        "",
-        "",
+        '신한은행 웹사이트를 리디자인하여 재구성한 웹사이트 입니다.',
+        '',
+        '',
       ],
-      stack: "HTML, CSS, SCSS, JavaScript",
-      directory: "external",
+      stack: 'HTML, CSS, SCSS, JavaScript',
+      directory: 'external',
     },
   ];
 
   const filteredProjects =
-    filter === "all"
+    filter === 'all'
       ? projects
       : projects.filter((project) => project.type === filter);
 
-  console.log("filteredProjects", filteredProjects);
+  console.log('filteredProjects', filteredProjects);
 
   const [currentSlide, setCurrentSlide] = useState({});
 
@@ -151,43 +151,43 @@ const Portfolio = () => {
   };
 
   return (
-    <div className="portfolio-container">
-      <div className="heading">
+    <div className='portfolio-container'>
+      <div className='heading'>
         <h2>My Work</h2>
-        <span className="icon"></span>
+        <span className='icon'></span>
       </div>
-      <div className="menu">
+      <div className='menu'>
         <button
-          className={filter === "all" ? "active" : ""}
-          onClick={() => setFilter("all")}
+          className={filter === 'all' ? 'active' : ''}
+          onClick={() => setFilter('all')}
         >
           전체 ({projects.length})
         </button>
         <button
-          className={filter === "personal" ? "active" : ""}
-          onClick={() => setFilter("personal")}
+          className={filter === 'personal' ? 'active' : ''}
+          onClick={() => setFilter('personal')}
         >
-          개인 프로젝트 ({projects.filter((p) => p.type === "personal").length})
+          개인 프로젝트 ({projects.filter((p) => p.type === 'personal').length})
         </button>
         <button
-          className={filter === "collaborative" ? "active" : ""}
-          onClick={() => setFilter("collaborative")}
+          className={filter === 'collaborative' ? 'active' : ''}
+          onClick={() => setFilter('collaborative')}
         >
           참여 프로젝트 (
-          {projects.filter((p) => p.type === "collaborative").length})
+          {projects.filter((p) => p.type === 'collaborative').length})
         </button>
       </div>
       {filteredProjects.map((project) => (
-        <div className="work-item" key={project.id}>
-          <div className="left">
-            {project.type === "collaborative" ? (
-              <div className="slider-container">
-                <div className="img-wrap">
+        <div className='work-item' key={project.id}>
+          <div className='left'>
+            {project.type === 'collaborative' ? (
+              <div className='slider-container'>
+                <div className='img-wrap'>
                   {project.images.map((image, index) => (
                     <div
                       key={index}
                       className={`img ${project.id}${index + 1} ${
-                        currentSlide[project.id] === index ? "active" : ""
+                        currentSlide[project.id] === index ? 'active' : ''
                       }`}
                     ></div>
                   ))}
@@ -195,68 +195,68 @@ const Portfolio = () => {
                 <button
                   // className="prev-btn"
                   className={`prev-btn ${
-                    currentSlide[project.id] === 0 && "disabled"
+                    currentSlide[project.id] === 0 && 'disabled'
                   }`}
                   onClick={() => prevSlide(project.id)}
                 >
-                  <span className="icon icon_prev"></span>
+                  <span className='icon icon_prev'></span>
                 </button>
                 <button
                   className={`next-btn ${
                     currentSlide[project.id] ===
                       projects.find((p) => p.id === project.id).images.length -
-                        1 && "disabled"
+                        1 && 'disabled'
                   }`}
                   onClick={() => nextSlide(project.id)}
                 >
-                  <span className="icon icon_next"></span>
+                  <span className='icon icon_next'></span>
                 </button>
               </div>
             ) : (
               <div className={`img ${project.id}`}></div>
             )}
           </div>
-          <div className="right">
-            <div className="project-info">
-              <div className="title-wrap">
-                <span className="text">프로젝트 명</span>
-                <span className="text">:</span>
-                <span className="text">{project.label}</span>
+          <div className='right'>
+            <div className='project-info'>
+              <div className='title-wrap'>
+                <span className='text'>프로젝트 명</span>
+                <span className='text'>:</span>
+                <span className='text'>{project.label}</span>
               </div>
-              <div className="category">
-                <span className="text">프로젝트 구분</span>
-                <span className="text">:</span>
-                <span className="text">
-                  {project.type === "personal"
-                    ? "개인 프로젝트"
-                    : "참여 프로젝트"}
+              <div className='category'>
+                <span className='text'>프로젝트 구분</span>
+                <span className='text'>:</span>
+                <span className='text'>
+                  {project.type === 'personal'
+                    ? '개인 프로젝트'
+                    : '참여 프로젝트'}
                 </span>
               </div>
-              <div className="desc">
-                <h4 className="title">프로젝트 내용</h4>
-                <div className="text-wrap">
-                  <p className="text">
+              <div className='desc'>
+                <h4 className='title'>프로젝트 내용</h4>
+                <div className='text-wrap'>
+                  <p className='text'>
                     {project.description.map((line, index) => (
-                      <p key={index} className="text">
+                      <p key={index} className='text'>
                         {line}
                       </p>
                     ))}
                   </p>
                 </div>
               </div>
-              <div className="skill">
-                <h4 className="title">사용 스택</h4>
-                <p className="text">{project.stack}</p>
+              <div className='skill'>
+                <h4 className='title'>사용 스택</h4>
+                <p className='text'>{project.stack}</p>
               </div>
             </div>
-            {project.type !== "collaborative" && (
-              <div className="submenu">
+            {project.type !== 'collaborative' && (
+              <div className='submenu'>
                 <button
-                  className="submenu-btn"
+                  className='submenu-btn'
                   onClick={() => {
-                    if (project.directory === "external") {
+                    if (project.directory === 'external') {
                       openExternalProject(
-                        `https://hys0327.github.io/js-project/${project.id}`,
+                        `https://hys0327.github.io/js-project/${project.id}/pages/home`,
                         `${project.label} project`,
                         1920,
                         1080
@@ -270,10 +270,10 @@ const Portfolio = () => {
                   Mobile View
                 </button>
                 <button
-                  className="submenu-btn"
+                  className='submenu-btn'
                   onClick={() => {
                     if (window.innerWidth <= 932) {
-                      alert("데스크탑 환경에서만 가능합니다.");
+                      alert('데스크탑 환경에서만 가능합니다.');
                     } else {
                       setSelectedWork(project.id);
                       setShowDesktopView(true);
